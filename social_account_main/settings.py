ff991f30847383ac88b8ac18cfd456ef3f0d15a5
kundanpandey11 <kundan.k.pandey02@gmail.com>
2023-09-23 19:19:31 +0530

    BASE_DIR AND DEBUG VAR IN ENV FILE

--- social_account_main/settings.py
+++ social_account_main/settings.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 import logging.config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_URL = "https://olduglyhomes.com"
+BASE_URL = config("BASE_URL", "https://olduglyhomes.com")
 # BASE_URL = "https://127.0.0.1:8000"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
