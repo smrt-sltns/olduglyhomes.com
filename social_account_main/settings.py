17a832325f790978460ba0d50688475e2fa5a7c4
kundanpandey11 <kundan.k.pandey02@gmail.com>
2024-01-20 19:27:39 +0530

    celery beat time rescheduled to normal

--- social_account_main/settings.py
+++ social_account_main/settings.py
@@ -289,11 +289,11 @@ CELERY_BEAT_SCHEDULE = {
 
       'add-every-2-hours': {
         'task': 'social_account_main.celery_task.task_every_2_hours',
-        # 'schedule': timedelta(hours=1),
-        'schedule': 300.0,
+        'schedule': timedelta(hours=1),
+        # 'schedule': 300.0,
         'args': '',
         'options': {
-            'expires': 120.0,
+            'expires': 150.0,
         },
       },
       
@@ -309,7 +309,7 @@ CELERY_BEAT_SCHEDULE = {
       #update ads spend every 10 min 
       'task_spend_limit': {
         'task': 'social_account_main.celery_task.task_spend_limit',
-        'schedule': 100.0,
+        'schedule': 600.0,
         'args': '',
         'options': {
             'expires': 150.0,
